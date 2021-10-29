cd40145056d62fdb50980f625495f7f8b0fce810
Alex Bailey <baileymk@hotmail.com>
2021-10-29 11:32:00 +0100

    added changing slides animation in 'get your favourites for free' section

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-const num1 = document.getElementById("num1")
-const num2 = document.getElementById("num2")
-const num3 = document.getElementById("num3")
-const num4 = document.getElementById("num4")
-const num5 = document.getElementById("num5")
+
 const bar = document.getElementById("bar")
 
-num1.addEventListener("click", barchanger() )
-
-
-this.innerHeight
 
 
 var button = document.querySelectorAll(".num").length;
@@ -16,13 +8,13 @@ var button = document.querySelectorAll(".num").length;
 for (let i = 0; i < button; i++) {
     document.querySelectorAll(".num")[i].addEventListener("click", function () {
            
-        // makesound(this.innerHTML)
+      
         barchanger(this.id)
        
     });
     
 }
-document.querySelectorAll(".set").onclick = playSound;
+
 
 
     function barchanger(input){
@@ -30,22 +22,65 @@ document.querySelectorAll(".set").onclick = playSound;
             case "num1":
                 // bar.style.width = 108 + "px"
                 bar.style.left = 450 + "px"
+                drinkcontainer = document.getElementsByClassName("drinkcontainer");
+                
+                for (i = 0; i < drinkcontainer.length; i++) {
+                  drinkcontainer[i].classList.remove("visible")
+                }      
+                  
+                  document.getElementById("coffee").classList.add("visible");
+                
                 break;
             case "num2":
                 // bar.style.width = 216 + "px"
                 bar.style.left = 558 + "px"
+                drinkcontainer = document.getElementsByClassName("drinkcontainer");
+                for (i = 0; i < drinkcontainer.length; i++) {
+                    drinkcontainer[i].classList.remove("visible")
+                const element = document.getElementById("bakery")
+                element.classList.add("visible");
+                
+                }      
+               
+                
+                
+
                 break;
             case "num3":
                 // bar.style.width = 324 + "px"
                 bar.style.left = 666 + "px"
+                drinkcontainer = document.getElementsByClassName("drinkcontainer");
+                for (i = 0; i < drinkcontainer.length; i++) {
+                    drinkcontainer[i].classList.remove("visible")
+                }      
+                
+                document.getElementById("handcrafted").classList.add("visible");
+                
+
                 break;
             case "num4":
                 // bar.style.width = 432 + "px"
                 bar.style.left = 774 + "px"
+                drinkcontainer = document.getElementsByClassName("drinkcontainer");
+                for (i = 0; i < drinkcontainer.length; i++) {
+                    drinkcontainer[i].classList.remove("visible")
+                }      
+              
+                document.getElementById("salad").classList.add("visible");
+                
+
                 break;
             case "num5":
                 // bar.style.width = 540 + "px"
                 bar.style.left = 882 + "px"
+                drinkcontainer = document.getElementsByClassName("drinkcontainer");
+                for (i = 0; i < drinkcontainer[i].length; i++) {
+                    drinkcontainer.classList.remove("visible")
+                }
+      
+                
+                document.getElementById("merch").classList.add("visible");
+                
                 break;
 
 
